9d2ebb5eb65ea43bef8ed51418c31d8fca105627
yafinoor <mnyafi777@gmail.com>
2017-11-05 19:33:11 +0700

    menampilkan dan menghitung persegi panjang

--- praktikum02.cpp
+++ praktikum02.cpp
@@ -5,4 +5,10 @@ int main(){
 	int luas,lebar,alas,tinggi;
 	char bidang;
 	cout << "jenis bidang (P atau S) ? "; cin >> bidang;
+	if (bidang=='P'){
+		cout <<"nilai tinggi = "; cin >> tinggi;
+		cout << "nilai lebar = "; cin >> lebar;
+		luas = tinggi*lebar;
+		cout << "luas persegi panjang adalah " << luas;
+	}
 }
