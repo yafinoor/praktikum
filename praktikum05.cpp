e9aaea6b41c0106e989ed3fbc731686fa3b33be3
yafinoor <mnyafi777@gmail.com>
2017-11-05 21:17:11 +0700

    Masukkan Tinggi dan Berat badan

--- praktikum05.cpp
+++ praktikum05.cpp
@@ -3,4 +3,7 @@ using namespace std;
 
 int main(){
 	
+	int tinggi,berat;
+	cout << "Masukkan Berapa Tinggi Badan Anda = "; cin >> tinggi;
+	cout << "Masukkan Berapa Berat Badan Anda = "; cin >> berat;
 }
