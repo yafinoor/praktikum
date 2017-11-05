a77a1cb3aeed952761fd3a52c2e99264322eea9a
yafinoor <mnyafi777@gmail.com>
2017-11-05 19:20:20 +0700

    percabangan untuk menentukan apakah x>y

--- praktikum01.cpp
+++ praktikum01.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main(){
 	
-	int x,y;
+	int x,y,z;
 	cout << "nilai x = "; cin >> x;
 	cout << "nilai y = "; cin >> y;
+	
+	if (x>y){
+		z=x-y;
+		cout << "x-y =" << z;
+	}
 }
